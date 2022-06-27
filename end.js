f87da91acd3bef8f1cdc95b3a341f35eafe274f4
GriffinSar <caladoniasaz@gmail.com>
2022-06-27 14:50:29 +0000

    Add Name Generator

--- end.js
+++ end.js
@@ -3,12 +3,31 @@ const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 finalScore.innerText = mostRecentScore;
-
-username.addEventListener('keyup', () => {
-    saveScoreBtn.disabled = !username.value;
-});
+const fName = document.querySelector(".fname");
+const lName = document.querySelector(".lname");
+const nameBTN = document.querySelector(".name-button");
 
 saveHighScore = (e) => {
     e.preventDefault();
 };
+
+const firstName = [
+    "Sarah", "Oisin", "Saoirse", "Eanna", "Frodo"]
+
+const lastName = [
+    "Walsh", "Griffin", "Leahy", "Phelan", "Smith"
+]
+
+
+nameBTN.addEventListener('click', function() {
+    fName.innerHTML =
+    firstName[Math.floor(Math.random() * firstName.length)];
+
+    lName.innerHTML = 
+    lastName[Math.floor(Math.random() * lastName.length)];
+});
+
+
+
+
 
